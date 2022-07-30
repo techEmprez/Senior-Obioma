beebea6867e2336d479f87155a4068893dea477f
techEmprez <suigeneriz001@gmail.com>
2022-07-30 11:33:41 +0100

    Add hamburger to mobile view

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
+const hamburger = document.querySelector(".hamburger");
+// const HamBurger = document.querySelector('.ham-container');
+
+hamburger.addEventListener("click", () => {
+  document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myHamburger").classList.toggle("change");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    document.getElementById("myDropdown").classList.remove("show");
+    document.getElementById("myHamburger").classList.remove("change");
+  })
+);
+
+
+
 const gallery = [
   {
     image: "./images/ankaraGown.jpg",
@@ -11,7 +28,6 @@ const gallery = [
     title: "Ankara Gowns",
     description: "This is a beautiful female ankara printed gown",
   },
- 
 ];
 const galleryCards = document.querySelector(".gallery-cards");
 gallery.forEach(function (card) {
